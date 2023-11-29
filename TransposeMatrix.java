139b4c818f2d50b869680d3e251c530e30966533
aman-5137 <amankkumaryadav5137@gmail.com>
2023-11-29 20:11:04 +0530

    29/11/2023

---
+++ TransposeMatrix.java
@@ -0,0 +1,26 @@
+ public class TransposeMatrix {
+    public static void main(String[] args) {
+        int arr[][]={
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        System.out.println("Original matrix");
+        for (int i=0;i< arr.length;i++){
+            System.out.println();
+            for(int j=0; j<arr.length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+        }
+        System.out.println("\nTransposed matrix");
+        for (int i=0;i< arr.length;i++){
+            System.out.println();
+            for(int j=0; j<arr.length;j++){
+                System.out.print(arr[j][i]+" ");
+            }
+        }
+    }
+}
+ 
+    
+
